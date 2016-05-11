ba8ef508d786206df8552eb6e7c18c759b6ab500
madhu <phatak.dev@gmail.com>
2016-05-11 20:24:30 +0530

    catalog example added

---
+++ src/main/scala/com/madhukaraphatak/examples/sparktwo/CatalogExample.scala
@@ -0,0 +1,47 @@
+package com.madhukaraphatak.examples.sparktwo
+
+import org.apache.spark.sql.SparkSession
+
+
+/**
+  * Catalogue Example
+  */
+object CatalogExample {
+
+  def main(args: Array[String]) {
+
+    val sparkSession = SparkSession.builder.
+           master("local")
+           .appName("example")
+           .getOrCreate()
+
+
+    val df = sparkSession.read.csv("src/main/resources/sales.csv")
+    df.registerTempTable("sales")
+
+    //interacting with catalogue
+
+    val catalog = sparkSession.catalog
+
+    //print the databases
+
+    catalog.listDatabases().select("name").show()
+
+    // print all the tables
+
+    catalog.listTables().select("name").show()
+
+    // is cached
+    println(catalog.isCached("sales"))
+    df.cache()
+    println(catalog.isCached("sales"))
+
+    // drop the table
+    catalog.dropTempTable("sales")
+    catalog.listTables().select("name").show()
+
+    // list functions
+    catalog.listFunctions().select("name","description","className","isTemporary").show(100)
+  }
+
+}
