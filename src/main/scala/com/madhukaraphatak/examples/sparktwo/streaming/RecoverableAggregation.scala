a029c3855610a3fa2b32c073e5399e62d0488964
madhu <phatak.dev@gmail.com>
2017-08-17 15:45:02 +0530

    state recovery added

---
+++ src/main/scala/com/madhukaraphatak/examples/sparktwo/streaming/RecoverableAggregation.scala
@@ -0,0 +1,41 @@
+package com.madhukaraphatak.examples.sparktwo.streaming
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.types.{
+  DoubleType,
+  StringType,
+  StructField,
+  StructType
+}
+
+object RecoverableAggregation {
+
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder
+      .master("local")
+      .appName("example")
+      .getOrCreate()
+
+    val schema = StructType(
+      Array(StructField("transactionId", StringType),
+            StructField("customerId", StringType),
+            StructField("itemId", StringType),
+            StructField("amountPaid", DoubleType)))
+
+    //create stream from folder
+    val fileStreamDf = sparkSession.readStream
+      .option("header", "true")
+      .schema(schema)
+      .csv("/tmp/input")
+
+    val countDs = fileStreamDf.groupBy("customerId").sum("amountPaid")
+    val query =
+      countDs.writeStream
+        .format("console")
+        .option("checkPoint", "/tmp/checkpoint")
+        .outputMode(OutputMode.Complete())
+
+    query.start().awaitTermination()
+  }
+}
