824c89dd29d0e7b2f76b3028254422e8389e024b
madhu <phatak.dev@gmail.com>
2017-10-23 12:44:55 +0530

    fixes for session example

--- src/main/scala/com/madhukaraphatak/examples/sparktwo/streaming/SessionisationExample.scala
+++ src/main/scala/com/madhukaraphatak/examples/sparktwo/streaming/SessionisationExample.scala
@@ -1,20 +1,19 @@
 package com.madhukaraphatak.examples.sparktwo.streaming
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout}
+import org.apache.spark.sql.streaming.{ GroupState, GroupStateTimeout }
 
 import scala.util.Try
 
 case class Session(sessionId: String, value: Double, endSignal: Option[String])
 
 case class SessionInfo(
-                        totalSum: Double)
+  totalSum: Double)
 
 case class SessionUpdate(
-                          id: String,
-                          totalSum: Double,
-                          expired: Boolean)
-
+  id:       String,
+  totalSum: Double,
+  expired:  Boolean)
 
 object SessionisationExample {
 
@@ -34,7 +33,7 @@ object SessionisationExample {
     val socketDs = socketStreamDf.as[String]
 
     // events
-    val events = socketDs.map(line => {
+    val events = socketDs.map(line ⇒ {
       val columns = line.split(",")
       val endSignal = Try(Some(columns(2))).getOrElse(None)
       Session(columns(0), columns(1).toDouble, endSignal)
@@ -42,27 +41,27 @@ object SessionisationExample {
 
     val sessionUpdates = events.groupByKey(_.sessionId)
       .mapGroupsWithState[SessionInfo, SessionUpdate](GroupStateTimeout.NoTimeout()) {
-      case (sessionId: String, eventsIter: Iterator[Session], state: GroupState[SessionInfo]) =>
-        val events = eventsIter.toSeq
-        val updatedSession = if (state.exists) {
-          val existingState = state.get
-          val updatedEvents = SessionInfo(existingState.totalSum + events.map(event => event.value).reduce(_ + _))
-          updatedEvents
-        }
-        else {
-          SessionInfo(events.length)
-        }
-        state.update(updatedSession)
-        //check did we get end signal or not
-        val isEndSignal = events.filter(value => value.endSignal.isDefined).length > 0
-        if (isEndSignal) {
-          state.remove()
-          SessionUpdate(sessionId, updatedSession.totalSum, true)
-        }
-        else {
-          SessionUpdate(sessionId, updatedSession.totalSum, false)
-        }
-    }
+        case (sessionId: String, eventsIter: Iterator[Session], state: GroupState[SessionInfo]) ⇒
+          val events = eventsIter.toSeq
+          val updatedSession = if (state.exists) {
+            val existingState = state.get
+            val updatedEvents = SessionInfo(existingState.totalSum + events.map(event ⇒ event.value).reduce(_ + _))
+            updatedEvents
+          }
+          else {
+            SessionInfo(events.map(event => event.value).reduce(_+_))
+          }
+          state.update(updatedSession)
+          //check did we get end signal or not
+          val isEndSignal = events.filter(value ⇒ value.endSignal.isDefined).length > 0
+          if (isEndSignal) {
+            state.remove()
+            SessionUpdate(sessionId, updatedSession.totalSum, true)
+          }
+          else {
+            SessionUpdate(sessionId, updatedSession.totalSum, false)
+          }
+      }
 
     val query = sessionUpdates
       .writeStream
@@ -71,7 +70,6 @@ object SessionisationExample {
       .start()
 
     query.awaitTermination()
-
 
   }
 }
